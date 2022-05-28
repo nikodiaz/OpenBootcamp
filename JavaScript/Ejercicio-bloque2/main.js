bba8430278b368cc8ba5749a2f7c86f71e43f4a1
Nicolás Díaz <nikodiaz56@gmail.com>
2022-05-28 17:50:25 -0300

    Ejercicio del bloque 2 del curso JS

---
+++ JavaScript/Ejercicio-bloque2/main.js
@@ -0,0 +1,12 @@
+const mi_obj = {
+  nombre: 'Nicolás Díaz',
+  edad: 25,
+  desarrollador: true,
+  'fecha-nacimiento': new Date('july 18 1996'),
+  'libro-favorito': {
+    título: 'En las montañas de la locura',
+    autor: 'H.P. Lovecraft',
+    fecha: 1936,
+    url: 'https://www.amazon.es/las-monta%C3%B1as-locura-Club-Di%C3%B3genes/dp/8477026181'
+  }
+}
