0327ff65668df77a453e4e33b50a54f936c6fce9
Nicolás Díaz <nikodiaz56@gmail.com>
2022-05-29 18:02:32 -0300

    Ejercicio del bloque 5 del curso de JS de OB

---
+++ JavaScript/Ejercicio-bloque5/main.js
@@ -0,0 +1,8 @@
+let altura = 171;
+let alturaMts = 1.71;
+let pesoKG = 82.7;
+let alturaRedondeada = Math.round(alturaMts);
+let pesoRedondeado = Math.floor(pesoKG);
+let comparacion = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+console.log(comparacion);
