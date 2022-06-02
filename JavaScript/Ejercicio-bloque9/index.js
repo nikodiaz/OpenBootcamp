d05146e2205382d92e31310d47983d9579e2accb
Nicolás Díaz <nikodiaz56@gmail.com>
2022-06-02 17:43:08 -0300

    Ejercicio del bloque 10 del curso de JS de OB

--- JavaScript/Ejercicio-bloque9/index.js
+++ JavaScript/Ejercicio-bloque9/index.js
@@ -12,8 +12,7 @@
      // - Write all logs with importance level of `info` or less to `combined.log`
      //
      new winston.transports.Console(),
-     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-     new winston.transports.File({ filename: 'combined.log' }),
+     new winston.transports.File({ filename: 'error.log', level: 'error'}),
    ],
  });
  
