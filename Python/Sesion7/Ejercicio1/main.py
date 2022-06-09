f9c8da6eb8ecd3d35fbee2643f7b10cbbe070f62
Nicolás Díaz <nikodiaz56@gmail.com>
2022-06-09 20:29:59 -0300

    Ejercicio 1 de la sesión 7 del curso de Python

---
+++ Python/Sesion7/Ejercicio1/main.py
@@ -0,0 +1,10 @@
+import operaciones as op
+
+def main():
+  print(op.suma(2,2))
+  print(op.resta(4,2))
+  print(op.dividir(2,2))
+  print(op.multiplicar(2,2))
+
+if __name__ == '__main__':
+  main()
